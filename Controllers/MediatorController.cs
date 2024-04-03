d3bf73f9e685cd90cc0555587670fff57d66a360
lofti198 <isolar2005@gmail.com>
2024-04-03 08:08:41 +0300

    added bot message

--- Controllers/MediatorController.cs
+++ Controllers/MediatorController.cs
@@ -2,6 +2,8 @@
 using Newtonsoft.Json;
 using ReadVideo.Server.Data;
 using System.Net;
+using System.Text;
+using System.Text.Json;
 
 namespace ReadVideo.Server.Controllers
 {
@@ -21,6 +23,7 @@ namespace ReadVideo.Server.Controllers
         [HttpPost("StartSpeaking")]
         public async Task<IActionResult> StartSpeaking([FromBody] ClientMessage chatRequest)
         {
+            // https://www.jivo.ru/docs/bot/
             // Create the response object and populate it
             var botResponse = new BotResponse
             {
@@ -31,14 +34,43 @@ namespace ReadVideo.Server.Controllers
             };
 
             // Setting the message details
+            botResponse.Message.Type = "TEXT";
             botResponse.Message.Text = "Я Mock! Чем могу вам помочь?";
             botResponse.Message.Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds(); // Current timestamp
 
+            // Serialize the BotResponse object to a JSON string using System.Text.Json
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsonResponse = System.Text.Json.JsonSerializer.Serialize(botResponse, options);
             // Serialize the BotResponse object to a JSON string
-            var jsonResponse = JsonConvert.SerializeObject(botResponse);
+            // var jsonResponse = JsonConvert.SerializeObject(botResponse);
 
-            // Return the serialized JSON string in the response
-            return Ok(jsonResponse);
+            // Initialize HttpClient
+            using (var httpClient = new HttpClient())
+            {
+                // Set the endpoint URL
+                var url = "https://bot.jivosite.com/webhooks/t1iWHhKC6aSYEgz/startspeaking";
+
+                // Create the HttpContent for the request
+                var content = new StringContent(jsonResponse, Encoding.UTF8, "application/json");
+
+                // Send the POST request
+                var response = await httpClient.PostAsync(url, content);
+
+                // Check the response status
+                if (response.IsSuccessStatusCode)
+                {
+                    // Handle success
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    // Do something with the response, if needed
+                    return Ok(responseContent);
+                }
+                else
+                {
+                    // Handle failure
+                    return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+                }
+            }
+
         }
     }
 }
