cb7d56722ace4a62c19be4f383dd0fdb59bd70de
lofti198 <isolar2005@gmail.com>
2024-04-08 12:37:29 +0300

    t

--- Services/IJivoService.cs
+++ Services/IJivoService.cs
@@ -28,7 +28,7 @@
                 Message = new BotMessage
                 {
                     Text = messageText,
-                    Type = "MARKDOWN",
+                    Type = "TEXT",
                     Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
                 },
                 Event = "BOT_MESSAGE"
