41300ecae1d0260a5b4261a7c9de96202ae623d6
lofti198 <isolar2005@gmail.com>
2024-04-08 17:49:07 +0300

    log client id and chat id

--- Controllers/MediatorController.cs
+++ Controllers/MediatorController.cs
@@ -64,8 +64,9 @@ namespace ReadVideo.Server.Controllers
 
         public async Task<IActionResult> JivoMediator(ClientMessage clientMessage, string assistantId)
         {
-            Console.WriteLine($"Call JivoMediator assistantId = {assistantId}, ChatId = {clientMessage.ChatId}, Message = {clientMessage.Message.Text}");
-            Debug.WriteLine($"Call JivoMediator assistantId = {assistantId}, ChatId = {clientMessage.ChatId}, Message = {clientMessage.Message.Text}");
+            string log = $"Call JivoMediator assistantId = {assistantId}, ChatId = {clientMessage.ChatId},ClientId = {clientMessage.ClientId}, Message = {clientMessage.Message.Text}";
+            Console.WriteLine(log);
+            Debug.WriteLine(log);
 
             // Immediately return OK result
             Task.Run(() => ProcessMessageInBackground(clientMessage));
