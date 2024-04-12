e4a34cde1aa28c76426823cd467d3f6674e40bfd
lofti198 <isolar2005@gmail.com>
2024-04-12 04:13:11 +0300

    в

--- Controllers/MediatorController.cs
+++ Controllers/MediatorController.cs
@@ -223,10 +223,11 @@ namespace ReadVideo.Server.Controllers
                             await jivoSiteService.SendMessageAsync(clientMessage.ClientId, clientMessage.ChatId, assistantResponse);
                             await Task.Delay(1000);
                             await jivoSiteService.SendMessageWithButtonsAsync(clientMessage.ClientId, clientMessage.ChatId,
-                                "Удалось ли найти ответ? Если нет, то могу переслать вопрос на email поддержки или показать похожие статьи из базы знаний. Также, вы можете сформулировать вопрос по-другому.", "text",
+                                "Удалось ли найти ответ? Если нет, то могу переслать вопрос на email поддержки или показать похожие статьи из базы знаний. Также, вы можете сформулировать вопрос по-другому либо сразу описать свою задачу по парсингу.", "text",
                                 new List<Button>() {
                             new Button() { Text = "Переслать поддержке", Id = 1 },
-                            new Button() { Text = "Показать похожие", Id = 20 }
+                            new Button() { Text = "Показать похожие", Id = 20 },
+                            new Button() { Text = "Описать задачу по парсингу", Id = 10 }
                                 });
                         }
 
