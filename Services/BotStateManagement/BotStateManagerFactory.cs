a70ddc7e50180635edda4fce057c547f427bfaf8
lofti198 <isolar2005@gmail.com>
2024-04-13 12:12:16 +0300

    complete datacol scenario first test within states

--- Services/BotStateManagement/BotStateManagerFactory.cs
+++ Services/BotStateManagement/BotStateManagerFactory.cs
@@ -99,7 +99,7 @@ namespace ReadVideo.Server.Services.BotStateManagement
                             }
                         ));
                     stateCollection.AddState(
-                        // Show similar knowledgebase links
+                        // BUtton "Show similar knowledgebase links" handler
                         new BotState(
                             async (clientToBotMessage, chatHistory) => clientToBotMessage.ButtonId == 20
                             ,
@@ -141,12 +141,72 @@ namespace ReadVideo.Server.Services.BotStateManagement
                                     "Перейдите, пожалуйста [по ссылке](https://web-data-extractor.net/buy/). Есть ли у вас еще вопросы?");
                             }
                         ));
-                    stateCollection.AddState(new BotState(
+                    stateCollection.AddState(
+                        // Other messages
+                        new BotState(
                             async (clientToBotMessage, chatHistory) => true
                             ,
                             async (clientToBotMessage, chatHistory) => {
-                             
-                                await _jivoSiteServiceFactory.GetOrCreate(key).SendMessageAsync(clientToBotMessage.ClientId, clientToBotMessage.ChatId, "Default case");
+                                var jivoSiteService = _jivoSiteServiceFactory.GetOrCreate(key);
+                                // Check if specific message hello
+                                var messageFeatures = await _messageEvaluator.EvaluateMessageFeatures(clientToBotMessage.Text);
+                                // Critical
+                                if (messageFeatures.Critical)
+                                {
+                                    await jivoSiteService.SendMessageAsync(clientToBotMessage.ClientId, clientToBotMessage.ChatId,
+                                        "Данный вопрос относится к срочным, поэтому передаю его сразу в поддержку! Наша команда свяжется с вами в течение 24 часов. Есть ли у вас еще вопросы?");
+                                    await _emailSenderServiceFactory.GetOrCreate(key).SendEmailAsync("isolar2005@gmail.com",
+                                        $"{key} Sensitive from client", clientToBotMessage.Text);
+                                  
+                                }
+                                // Just hello
+                                else if (!messageFeatures.Complex)
+                                {
+                                   
+                                    await jivoSiteService.SendMessageWithButtonsAsync(clientToBotMessage.ClientId, clientToBotMessage.ChatId,
+                                     "Я AI помощник Datacol. Пожалуйста, задайте свой вопрос. Также, возможно, вы сразу хотите: ", "text",
+                                     new List<Button>() {
+                                                    new Button() { Text = "Описать задачу по парсингу", Id = 10 },
+                                                    new Button() { Text = "Купить программу", Id = 50 }
+                                     });
+                                }
+                                else
+                                {
+                                    await Task.Delay(1000);
+                                    await jivoSiteService.SendMessageAsync(clientToBotMessage.ClientId, clientToBotMessage.ChatId, "Пару секунд, AI готовит ответ...");
+
+                                    List<FAQItem> faqLinks = await _embeddingManager.GetResponseAsync(clientToBotMessage.Text);
+                                    clientToBotMessage.FaqItems = faqLinks;
+                                    //await _assistant.GetResponseAsync(clientMessage.Message.Text,"", Consts.OpenAIAssistantID_DC, clientMessage.ChatId);
+
+                                    string assistantResponse = await _assistant.GetResponseAsync(clientToBotMessage.Text,
+                                        faqLinks.BuildAssistantInstruction(),
+                                        Consts.OpenAIAssistantID_DC, clientToBotMessage.ChatId);
+
+                                    await jivoSiteService.SendMessageAsync(clientToBotMessage.ClientId, clientToBotMessage.ChatId, assistantResponse);
+                                    await Task.Delay(1000);
+
+                                    // в базе знаний нет
+                                    if (assistantResponse.ToLower().Contains("в базе знаний нет"))
+                                    {
+                                        await jivoSiteService.SendMessageWithButtonsAsync(clientToBotMessage.ClientId, clientToBotMessage.ChatId,
+                                            "Могу переслать вопрос на email поддержки. Также, вы можете сформулировать вопрос по-другому.", "text",
+                                            new List<Button>() {
+                                                new Button() { Text = "Переслать поддержке", Id = 1 },
+                                            });
+                                    }
+                                    else
+                                    {
+
+                                        await jivoSiteService.SendMessageWithButtonsAsync(clientToBotMessage.ClientId, clientToBotMessage.ChatId,
+                                            "Удалось ли найти ответ? Если нет, то могу переслать вопрос на email поддержки или показать похожие статьи из базы знаний. Также, вы можете сформулировать вопрос по-другому либо сразу описать свою задачу по парсингу.", "text",
+                                            new List<Button>() {
+                                                new Button() { Text = "Переслать поддержке", Id = 1 },
+                                                new Button() { Text = "Показать похожие", Id = 20 },
+                                                new Button() { Text = "Описать задачу по парсингу", Id = 10 }
+                                            });
+                                    }
+                                }
 
                             }
                         ));
