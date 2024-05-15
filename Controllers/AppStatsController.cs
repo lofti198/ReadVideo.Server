203931452012798f8864e5b9682b3da02eba895e
lofti198 <isolar2005@gmail.com>
2024-05-15 07:39:12 +0300

    extract domain from stats, protect with api key

--- Controllers/AppStatsController.cs
+++ Controllers/AppStatsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ReadVideo.Server.Data;
+using ReadVideo.Server.Middleware.Attributes;
 using ReadVideo.Server.Models;
 
 namespace ReadVideo.Server.Controllers
@@ -24,9 +25,13 @@ namespace ReadVideo.Server.Controllers
                 return BadRequest("No data provided");
             }
 
+
+            string domain = ExtractDomain(dataDto.Domain);
+
+
             // Check if an entry with the same email and domain already exists
             var existingEntry = _context.CampaignLaunchStats
-                                .Any(x => x.Email == dataDto.Email && x.Domain == dataDto.Domain);
+                                .Any(x => x.Email == dataDto.Email && x.Domain == domain);
 
             if (existingEntry)
             {
@@ -36,7 +41,7 @@ namespace ReadVideo.Server.Controllers
             var data = new CampaignLaunchStatsElement
             {
                 Email = dataDto.Email,
-                Domain = dataDto.Domain
+                Domain = domain
             };
 
             _context.CampaignLaunchStats.Add(data);
@@ -46,10 +51,35 @@ namespace ReadVideo.Server.Controllers
         }
 
         [HttpGet("view-all")]
-        public async Task<IActionResult> GetAllRecords()
+        [ApiKey]
+        public async Task<IActionResult> GetAllRecords([FromQuery] int delete = 0)
         {
             var data = await _context.CampaignLaunchStats.ToListAsync();
+
+            if (delete == 1)
+            {
+                _context.CampaignLaunchStats.RemoveRange(_context.CampaignLaunchStats);
+                await _context.SaveChangesAsync();
+            }
+
             return Ok(data);
+        }
+
+        //[HttpGet("view-all")]
+        //public async Task<IActionResult> GetAllRecords()
+        //{
+        //    var data = await _context.CampaignLaunchStats.ToListAsync();
+        //    return Ok(data);
+        //}
+
+
+        private string ExtractDomain(string input)
+        {
+            if (Uri.TryCreate(input, UriKind.Absolute, out Uri uri))
+            {
+                return uri.Host;
+            }
+            return input;
         }
     }
 
---
+++ Middleware/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ReadVideo.Server.Middleware.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName = "X-Api-Key";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var apiKey = Environment.GetEnvironmentVariable("X_API_KEY");
+
+            if (String.IsNullOrEmpty(apiKey) || !apiKey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+
+}
